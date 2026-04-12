53d7399bac33556b9a4640f346ccae4a2a42934d
vpwinvest <hello@vpwinvest.com>
2026-04-12 15:52:42 +0100

    Change base URL in Vite config to root

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-   base: "/vlington-brand-bloom/",
+   base: "/",
   server: {
     host: "::",
     port: 8080,
