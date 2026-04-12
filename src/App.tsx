8a4bd9c54677e774e517c2e13e689e0431f772ba
vpwinvest <hello@vpwinvest.com>
2026-04-12 15:47:55 +0100

    Set basename for BrowserRouter in App component

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter basename="/vlington-brand-bloom">
           <ScrollToTop />
           <Routes>
             <Route path="/" element={<Index />} />
